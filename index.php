c7f374d6d24a062b4b810aaad93cf93d5f371e21
laurabermejo <laura.ber.car@gmail.com>
2017-12-20 15:16:07 +0100

    creado .php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8"/>
+        <title>BananaTube</title>
+        <link rel="stylesheet" href="includes/footer.css" media="all"/>
+    </head>
+        <body>
+            <?php include "includes/footer.html";?>
+        </body>
+</html>
