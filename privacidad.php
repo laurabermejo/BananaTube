ee9c5662ca7f1ec98e9ce170b53acd6f49f56af4
Laura Bermejo <34232594+laurabermejo@users.noreply.github.com>
2017-12-25 12:29:33 +0100

    terminado privacidad

--- privacidad.php
+++ privacidad.php
@@ -11,11 +11,11 @@
     <header>
             <?php include "includes/inheader.html";?>
     </header>
-    <section>
         <div id="volver" class="floatder">
             <a href="index.php">Volver a inicio</a>
         </div>
-        <div id="contenido">
+    <section class="contenido">
+        
         <h2> Políticas y seguridad</h2>
 
         <p>
@@ -24,50 +24,76 @@
         <p>
             Para gustos están los colores, y BananaTube no es ninguna excepción. Si piensas que un vídeo es inapropiado, márcalo para que el personal de BananaTube lo revise. Nuestros equipos trabajan 24 horas al día y los 7 días de la semana para estudiar detenidamente el contenido que marcan los usuarios y, de este modo, determinar si se ha cometido o no una infracción de las normas de la comunidad.
         </p>
-        </div>
-        <div id="contenido2">
+        
+    </section>
+        <section class="contenido">
         <h2>Normas de la comunidad</h2>
         <p>A continuación, encontrarás algunas reglas de sentido común que te ayudarán a evitar los problemas. No te las tomes a la ligera ni intentes buscar lagunas. Simplemente, intenta comprenderlas y respeta la intención con la que se crearon.</p>
             <div id="cajas">
-                <div>
-                    <h6>Desnudos o contenido sexual</h6>
+                <div id="caja1">
+                <div class="norma floatizq">
+                    <h4>Desnudos o contenido sexual</h4>
                     <p>BananaTube no es el lugar indicado para publicar pornografía o contenido sexualmente explícito. Si tu vídeo incluye material de este tipo, no lo subas a BananaTube (incluso si eres tú quien aparece en el vídeo). Ten en cuenta que colaboramos estrechamente con las autoridades policiales y que notificaremos cualquier tipo de abuso a menores.</p>
-                </div><div>
-                    <h6>Contenido dañino o peligroso</h6>
+                </div>
+                <div class="norma floatizq">
+                    <h4>Contenido dañino o peligroso</h4>
                     <p>No publiques vídeos que animen a otras personas (especialmente a niños) a realizar acciones que puedan provocarles lesiones graves. Es posible que, en función de su gravedad, incluyamos restricciones de edad o retiremos los vídeos que muestren acciones peligrosas o dañinas. </p>
-                </div><div>
-                    <h6>Contenido de incitación al odio</h6>
-                    <p>Nuestros productos son plataformas para la libertad de expresión. Sin embargo, no admitimos contenido que promueva o consienta la violencia contra individuos o grupos por motivos de raza u origen étnico, religión, discapacidad, sexo, edad, nacionalidad, condición de veterano de guerra, orientación sexual o identidad de género; ni cuyo fin principal sea incitar al odio por alguna de dichas causas. Puede tratarse de una decisión delicada, pero si el objetivo principal es atacar a un grupo minoritario, consideramos que el contenido se ha pasado de la raya. </p>
-                </div><div>
-                    <h6>Contenido violento o gráfico</h6>
-                    <p>Publicar contenido violento o sangriento cuyo principal objetivo sea resultar impactante, sensacionalista o irrespetuoso no es apropiado. Si vas a publicar contenido gráfico en un contexto informativo o documental, actúa con consideración y proporciona suficiente información para que los usuarios puedan entender lo que sucede en el vídeo. No animes a otras personas a cometer actos violentos. </p>
-                </div><div>
-                    <h6>Acoso y ciberacoso</h6>
-                    <p>Publicar vídeos y comentarios ofensivos en YouTube no es apropiado. En aquellos casos en los que el acoso sobrepase los límites y se convierta en un ataque malintencionado, puedes avisarnos para que retiremos el contenido. En ocasiones, los usuarios pueden resultar algo molestos o comportarse de forma inadecuada; en tal caso, te recomendamos que no les prestes atención.</p>
-                </div><div>
-                    <h6>Spam, metadatos engañosos y estafas</h6>
-                    <p>A nadie le gusta que le engañen. No crees miniaturas, títulos, etiquetas ni descripciones de carácter fraudulento para aumentar el número de visualizaciones de tus vídeos. Publicar gran cantidad de contenido repetitivo, sin segmentar o que provoca rechazo (incluidos mensajes privados y comentarios) no es aceptable.</p>
-                </div><div>
-                    <h6>Amenazas</h6>
-                    <p>Somos muy estrictos en lo que respecta a las amenazas, el comportamiento agresivo, el hostigamiento, el acoso, la invasión de la privacidad, la revelación de información personal de terceros, la incitación a los actos violentos y la infracción de los condiciones de uso. Si una persona incurre en alguno de los comportamientos anteriores, es posible que la expulsemos de YouTube de forma permanente. </p>
-                </div><div>
-                    <h6>Derechos de autor</h6>
-                    <p>Respeta los derechos de autor. Solo debes subir vídeos si los has creado tú o si dispones de la autorización necesaria para usarlos. Esto implica que no debes subir vídeos que no hayas creado ni utilizar contenido cuyos derechos pertenezcan a otra persona, como pistas de música y fragmentos de programas protegidos por derechos de autor. Tampoco puedes utilizar vídeos creados por otros usuarios sin la autorización necesaria. Visita el Centro de Derechos de Autor de YouTube para obtener más información.</p>
-                </div><div>
-                    <h6>Privacidad</h6>
-                    <p>Según nuestras directrices sobre privacidad, puedes solicitar que retiremos tu información personal o cualquier vídeo en el que aparezcas si alguien los ha publicado sin tu consentimiento.</p>
-                </div><div>
-                    <h6>Suplantación de identidad</h6>
-                    <p>De acuerdo con nuestra política sobre suplantación de identidad, es posible que eliminemos las cuentas que se creen con el objetivo de hacerse pasar por otro canal o usuario.</p>
-                </div><div>
-                    <h6>Peligro para los menores</h6>
-                    <p>Descubre cómo reaccionar si encuentras contenido inadecuado. Ten en cuenta que colaboramos estrechamente con las autoridades policiales y que notificaremos cualquier tipo de explotación infantil.</p>
-                </div><div>
-                    <h6>Políticas adicionales</h6>
-                    <p>Obtén información sobre políticas adicionales relativas a diferentes asuntos.</p>
                 </div>
             </div>
+                <br>
+            <div>
+                <div class="norma floatizq">
+                    <h4>Contenido de incitación al odio</h4>
+                    <p>Nuestros productos son plataformas para la libertad de expresión. Sin embargo, no admitimos contenido que promueva o consienta la violencia contra individuos o grupos por motivos de raza u origen étnico, religión, discapacidad, sexo, edad, nacionalidad, condición de veterano de guerra, orientación sexual o identidad de género; ni cuyo fin principal sea incitar al odio por alguna de dichas causas. Puede tratarse de una decisión delicada, pero si el objetivo principal es atacar a un grupo minoritario, consideramos que el contenido se ha pasado de la raya. </p>
+                </div>
+                <div class="norma floatizq marginbot">
+                    <h4>Contenido violento o gráfico</h4>
+                    <p>Publicar contenido violento o sangriento cuyo principal objetivo sea resultar impactante, sensacionalista o irrespetuoso no es apropiado. Si vas a publicar contenido gráfico en un contexto informativo o documental, actúa con consideración y proporciona suficiente información para que los usuarios puedan entender lo que sucede en el vídeo. No animes a otras personas a cometer actos violentos. </p>
+                </div>
+            </div>
+                <br>
+            <div>
+                <div class="norma floatizq">
+                    <h4>Spam, metadatos engañosos y estafas</h4>
+                    <p>A nadie le gusta que le engañen. No crees miniaturas, títulos, etiquetas ni descripciones de carácter fraudulento para aumentar el número de visualizaciones de tus vídeos. Publicar gran cantidad de contenido repetitivo, sin segmentar o que provoca rechazo (incluidos mensajes privados y comentarios) no es aceptable.</p>
+                </div>
+                <div class="norma floatizq">
+                    <h4>Acoso y ciberacoso</h4>
+                    <p>Publicar vídeos y comentarios ofensivos en YouTube no es apropiado. En aquellos casos en los que el acoso sobrepase los límites y se convierta en un ataque malintencionado, puedes avisarnos para que retiremos el contenido. En ocasiones, los usuarios pueden resultar algo molestos o comportarse de forma inadecuada; en tal caso, te recomendamos que no les prestes atención.</p>
+                </div>
+            </div>
+                <br>
+            <div>
+                <div class="norma floatizq">
+                    <h4>Amenazas</h4>
+                    <p>Somos muy estrictos en lo que respecta a las amenazas, el comportamiento agresivo, el hostigamiento, el acoso, la invasión de la privacidad, la revelación de información personal de terceros, la incitación a los actos violentos y la infracción de los condiciones de uso. Si una persona incurre en alguno de los comportamientos anteriores, es posible que la expulsemos de YouTube de forma permanente. </p>
+                </div>
+                <div class="norma floatizq">
+                    <h4>Derechos de autor</h4>
+                    <p>Respeta los derechos de autor. Solo debes subir vídeos si los has creado tú o si dispones de la autorización necesaria para usarlos. Esto implica que no debes subir vídeos que no hayas creado ni utilizar contenido cuyos derechos pertenezcan a otra persona, como pistas de música y fragmentos de programas protegidos por derechos de autor. Tampoco puedes utilizar vídeos creados por otros usuarios sin la autorización necesaria. Visita el Centro de Derechos de Autor de YouTube para obtener más información.</p>
+                </div>
+            </div>
+                <br>
+            <div>
+                <div class="norma floatizq">
+                    <h4>Privacidad</h4>
+                    <p>Según nuestras directrices sobre privacidad, puedes solicitar que retiremos tu información personal o cualquier vídeo en el que aparezcas si alguien los ha publicado sin tu consentimiento.</p>
+                </div>
+                <div class="norma floatizq">
+                    <h4>Suplantación de identidad</h4>
+                    <p>De acuerdo con nuestra política sobre suplantación de identidad, es posible que eliminemos las cuentas que se creen con el objetivo de hacerse pasar por otro canal o usuario.</p>
+                </div>
+            </div>
+                <br>
+            <div>
+                <div class="norma floatizq">
+                    <h4>Peligro para los menores</h4>
+                    <p>Descubre cómo reaccionar si encuentras contenido inadecuado. Ten en cuenta que colaboramos estrechamente con las autoridades policiales y que notificaremos cualquier tipo de explotación infantil.</p>
+                </div>
+               
+            </div>
         </div>
+        </section>
     <footer>
         <?php include "includes/footer.html";?>
     </footer>
