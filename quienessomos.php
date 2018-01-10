43b878d219fccef17bf3b916927676032692e6f8
Laura Bermejo <34232594+laurabermejo@users.noreply.github.com>
2018-01-10 09:24:27 +0100

    unificación quiénes somos

---
+++ quienessomos.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8"/>
+        <title>Quiénes somos</title>
+        <link rel="stylesheet" href="css/quienessomos.css" media="all"/>
+    </head>
+    <body>
+        <section>
+          <div id="header"><?php include "includes/inheader.html";?></div>
+        </section>
+        <br>
+        <section>
+            <h3>Conócenos</h3>
+            <div>
+                <div>
+                    <img src="#" alt="Marian"/>
+                    <h4>Marian</h4>
+                    <p>BlaBlaBla1</p>
+                </div>
+                <div>
+                    <img src="#" alt="Laura"/>
+                    <h4>Laura</h4>
+                    <p>BlaBlaBla2</p>
+                </div>
+                <div>
+                    <img src="#" alt="Carlos"/>
+                    <h4>Carlos</h4>
+                    <p>BlaBlaBla3</p>
+                </div>
+                <div>
+                    <img src="#" alt="Borja"/>
+                    <h4>Borja</h4>
+                    <p>BlaBlaBla4</p>
+                </div>
+                <div>
+                    <img src="#" alt="Noemí"/>
+                    <h4>Noemí</h4>
+                    <p>BlaBlaBla5</p>
+                </div>
+            </div>
+        </section>
+        <br>
+        <section>
+            <div><?php include "includes/footer.html";?></div>
+        </section>
+    </body>
+</html>
