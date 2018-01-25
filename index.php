7dd7f23f4f869e683076fa55e93aa51abfee8b71
laurabermejo <laura.ber.car@gmail.com>
2018-01-25 14:36:59 +0100

    añadir bootstrap header

--- index.php
+++ index.php
@@ -3,7 +3,14 @@
     <head>
         <meta charset="utf-8"/>
         <title>BananaTube</title>
+        <link rel="stylesheet" href="css/bootstrap.css"/>
         <link rel="stylesheet" href="css/index.css" media="all"/>
+        <script src="js/jquery-3.3.1.slim.js"></script>
+        <script src="js/bootstrap.bundle.js"></script>
+        <script src="js/jquery.validate.js"></script>
+        <script src="js/additional-methods.js"></script>
+        <script src="js/messages_es.js"></script>
+        <script src=""></script>
     </head>
         <body>
             <div id="header"><?php include "includes/inheader.html";?></div>
