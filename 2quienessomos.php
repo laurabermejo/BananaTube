f7f4ba4befabada10355880d6d5b9f786b7ee71a
Mayabits <34232692+Mayabits@users.noreply.github.com>
2018-01-26 15:54:34 +0100

    maquetado 2quienessomos

---
+++ 2quienessomos.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8"/>
+        <title>Quiénes somos</title>
+         <link rel="stylesheet" href="css/bootstrap.css"/>
+    <link rel="stylesheet" href="css/quienessomos.css"/>
+    <script src="js/jquery-3.3.1.slim.js"></script>
+    <script src="js/bootstrap.bundle.js"></script>
+    <script src="js/jquery.validate.js"></script>
+    <script src="js/additional-methods.js"></script>
+    <script src="js/messages_es.js"></script>
+    <script src=""></script>
+    </head>
+    <body>
+       <!---->
+      <header class="row"><?php include "includes/inheader.html";?></header>
+        <hr>
+         <section class="container-fluid">
+            <div class="row">
+            <div id="toptittle" class="col-xl-12 text-center">
+                <h3>Conócenos</h3>
+            </div>
+                <div id="who" class="row">
+                    <p class="col-xl-4, offset-xl-3">¿De dónde venimos? ¿Quiénes somos? ¿Adónde vamos?</p>    
+                </div>
+            <div id="meetus" class="row">
+                <div class="col-xl-4">
+                    <center>
+                        <img id="marianspic" src="imagenes/monkeyface1.png" alt="Marian"/>
+                    </center>
+                    <h4>Marian</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.</p>
+                    </div>
+                <div class="col-xl-4">
+                    <center>
+                        <img id="lauraspic"src="imagenes/monkeyface2.png" alt="Laura"/>
+                    </center>
+                    <h4>Laura</h4>
+                        <p>In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.</p>
+                    </div>
+                <div class="col-xl-4">
+                    <center>
+                        <img id="noemispic"src="imagenes/monkeyface5.png" alt="Noemí"/>
+                    </center>
+                    <h4>Noemí</h4>
+                        <p>Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus.</p>
+                
+            </div>
+                 </div>
+                </div>
+        </section>  
+        <hr>
+        
+ <footer>
+     <nav id="izq" class="col-xl-3 col-lg-3 col-md-4 col-sm-4 col-4">
+    <ul>
+        <li><strong><a href="quienessomos.html">QUIÉNES SOMOS</a></strong></li>
+        <li><strong><a href="#">PREGUNTAS FRECUENTES</a></strong></li>
+        <li><strong><a href="contacto.php">CONTACTO</a></strong></li>
+    </ul>
+     </nav>
+        <div class="col-xl-3 offset-xl-6 col-lg-4 offset-lg-5 col-md-4 offset-md-4 col-sm-6 col-7 " id="dcha">
+        <div class="row text-right">
+        <div class="col-xl-12">
+            <a  href="http://twitter.com/">
+                        <img src="imagenes/socialmedia/twitter.png" 
+                             alt="Twitter"/></a>
+        
+            <a  href="http://facebook.com/">
+                        <img src="imagenes/socialmedia/facebook.png"
+                             alt="Facebook"/></a>
+        
+            <a  href="http://plus.google.com/">
+                        <img src="imagenes/socialmedia/google-plus.png"
+                             alt="Google+"/></a>
+      
+            <a href="http://www.instagram.com/">
+            
+                        <img src="imagenes/socialmedia/instagram.png"
+                             alt="Instagram"/></a>
+        
+            <a href="http://www.youtube.com/">
+                        <img src="imagenes/socialmedia/youtube.png"
+                             alt="YouTube"/></a>
+      
+                    </div>
+                </div>
+            <div class="row text-right">
+            <div class="col-xl-12"><strong>&copy;BANANATUBE 2017</strong></div>
+            </div>   
+        </div>
+     </footer>  
+    </body>
+</html>
