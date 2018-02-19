b04441fc3dde7b67d07a0f4ce5ef7d5991d46a5f
Noemi <noemi.perlopez@gmail.com>
2018-02-19 18:01:08 +0100

    implementando validación...

--- js/iniciarsesion.js
+++ js/iniciarsesion.js
@@ -1,1 +1,31 @@
-$("form").vindicate("init");
+function validar(){
+    var formularios=document.getElementByClassName("needs-validation");
+    var validacion= Array.protoype.filter.call(formularios, function(form){
+        form.addEventListener("submit", function(event){
+            if (form.checkValidity()===false){
+                event.preventDefault();
+                event.stopPropagation();
+            }else{
+            form.classList.add("was-validated");
+            }
+        }, false);
+    });
+}
+
+function validar(){
+  'use strict';
+  window.addEventListener('load', function() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+};
