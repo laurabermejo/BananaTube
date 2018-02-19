bae4c0fe9ebd7abbece4c94c62297ba1310f6581
Noemi <noemi.perlopez@gmail.com>
2018-02-19 13:35:35 +0100

    añadido fontawesome

--- reproduccion.php
+++ reproduccion.php
@@ -4,10 +4,11 @@
     <meta charset="utf-8">
     <title>Paseo por Roraima - BananaTube</title>
     <link rel="stylesheet" href="css/bootstrap.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
     <link rel="stylesheet" href="css/reproduccion.css"/>
     <script src="js/jquery-3.2.1.js"></script>
     <script src="js/bootstrap.js"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
+    
     <!-- fin iconos font-awesome -->
 
 </head>
