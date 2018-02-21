00f0c7e3febb823f972d33abc139f8248e46508f
Noemi <noemi.perlopez@gmail.com>
2018-02-21 17:56:14 +0100

    bbdd en index.js, comienzo maquetacion subirvideo.php

--- subirvideo.php
+++ subirvideo.php
@@ -8,75 +8,75 @@
         <link rel="stylesheet" href="css/subirvideo.css"/>
         <script src="js/jquery-3.2.1.js"></script>
         <script src="js/bootstrap.js"></script>
-        <script src="js/tether.min.js"></script>
-        <script src="js/vindicate.js"></script>
         <script src="js/subirvideo.js"></script>
     </head>
     <body>
-        <header class="row">
-            <?php include "includes/inheader.html";?>
-        </header>
-        <hr>
-        <section>
-            <form name="subirvideo"
-                  id="formsubir"
-                  action=""
-                  method="post"
-                  enctype="multipart/form-data">
-                <div>
-                    <label for="archivo"></label>
-                    <input type="file"
-                           name="archivo"
-                           id="archivo"
-                           value="video"
-                           multiple="multiple"
-                           required/>
-                </div>
-                <div>
-                    <label for="miniatura"></label>
-                    <input type="file"
-                           name="miniatura"
-                           value="miniatura"
-                           required/>
-                </div>
-                <div>
-                    <label for="nombrevid">Título</label>
-                    <input type="text"
-                           name="nombrevid"
-                           id="nombrevid"
-                           value=""
-                           maxlength="65"
-                           placeholder="Nombre de tu vídeo"
-                           required/>
-                </div>
-                <div>
-                    <label for="descvid">Descripción</label>
-                    <textarea name="descvid"
+        <div class="container-fluid margin">
+            <header class="row">
+                <?php include "includes/inheader.html";?>
+            </header>
+            <div id="contenedor" class="row">
+                <section class="">
+                    <form name="subirvideo"
+                      id="formsubir"
+                      action=""
+                      method="post"
+                      enctype="multipart/form-data">
+                        <div>
+                            <label for="archivo"></label>
+                            <input type="file"
+                               name="archivo"
+                               id="archivo"
+                               value="video"
+                               multiple="multiple"
+                               required/>
+                        </div>
+                        <div>
+                            <label for="miniatura"></label>
+                            <input type="file"
+                               name="miniatura"
+                               value="miniatura"
+                               required/>
+                        </div>
+                        <div>
+                            <label for="nombrevid">Título</label>
+                            <input type="text"
+                               name="nombrevid"
+                               id="nombrevid"
+                               value=""
+                               maxlength="65"
+                               placeholder="Nombre de tu vídeo"
+                               required/>
+                        </div>
+                        <div>
+                            <label for="descvid">Descripción</label>
+                            <textarea name="descvid"
                               id="descvid"
                               rows="5"
                               columns="5"
                               maxlenght="350"
                               placeholder="Describe tu vídeo"></textarea>
-                </div>
-                <div>
-                    <label for="tags">Etiquetas</label>
-                    <textarea name="tags"
+                        </div>
+                        <div>
+                            <label for="tags">Etiquetas</label>
+                            <textarea name="tags"
                               id="tags"
                               rows="5"
                               columns="5"
                               placeholder="Añade tags relacionados"></textarea>
-                </div>
-                <div>
-                    <input type="submit"
+                        </div>
+                        <div>
+                            <input type="submit"
                            name="submit"
                            id="submit"
                            value="Subir vídeo"/>
-                </div>
-            </form>
-        </section>
-        <hr>
-        <footer>
-           <?php include "includes/footer.html";?>
-        </footer>
+                        </div>
+                    </form>
+                </section>
+            </div>
+            <footer class="row">
+                <?php include "includes/footer.html";?>
+            </footer>
+        </div>
     </body>
 </html>
